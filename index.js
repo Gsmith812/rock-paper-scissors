18f1dd4392932fa264b366bfc3ddad0a1480e986
gsmith <georgesmith812@gmail.com>
2023-10-10 19:53:00 -0400

    Created playRound func and moved choices to global variable
    Incorporated playRound function using conditionals with comparison operators to return result of the round

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Create constant global variable for set of choices for game
+
+const choices = ['Rock', 'Paper', 'Scissors'];
+
 // Create getComputerChoice function
 // That will randomly return one of three strings
 // 'Rock', 'Paper', or 'Scissors' randomly
@@ -6,9 +10,38 @@
 // return computerChoice
 
 function getComputerChoice () {
-    let choices = ['Rock', 'Paper', 'Scissors'];
+
     let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice = choices[randomNumber];
     
     return computerChoice;
 }
+
+// Create playRound() function that takes two parameters
+// playerSelection and computerSelection
+// compare playerSelection with compterSelection to return either two strings
+// 'You win! playerSelection beats computerSelection!
+// OR 'You lose! computerChoice beats playerChoice!
+// OR 'You tied!'
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+    // Create index variables for playerSelection and computerSelection to reduce conditional size
+    let playerChoiceIndex = choices.indexOf(playerSelection);
+    let computerChoiceIndex = choices.indexOf(computerSelection);
+
+    //conditonals to handle results of round
+    //using comparison operators to compare index values of each selection in the choices array
+    if (playerChoiceIndex === computerChoiceIndex) {
+        result = `You Tied! You both selected ${playerSelection}`;
+    } else if ( ((playerChoiceIndex - 1) === computerChoiceIndex) || (computerChoiceIndex === 2 && playerChoiceIndex === 0) ) {
+        result = `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    // return result
+
+    return result;
+}
+
